5fa3094ab83f70d3b9619a2d7f9474c8fcd97ea1
Zach  Smith <zdsmith001@gmail.com>
2019-07-24 16:43:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+  if n == 2
+    true
+  elsif n <= 1 || n % 2 == 0
+    false
+  else
+    divisors = (3..Math.sqrt(n)).step(2)
+    divisors.none? { |d| n % d == 0 }
+  end
+end
